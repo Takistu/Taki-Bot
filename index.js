273f1da4694d7bbdfea5b927a0c5c4550b6e8229
Taki Eddine <takieddineelattari@gmail.com>
2026-02-16 21:31:17 +0000

    Second commit

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ setInterval(() => {
     }
 }, 30_000) // check every 30 seconds
 
-let phoneNumber = "911234567890"
+let phoneNumber = "212781204912"
 let owner = JSON.parse(fs.readFileSync('./data/owner.json'))
 
 global.botname = "Taki BOT"
